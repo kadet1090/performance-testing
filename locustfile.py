8c803208c187376732dacbe2ca94614dd9a53972
Kacper Donat <kadet1090@gmail.com>
2021-02-15 21:06:38 +0100

    Add metric collection system

--- locustfile.py
+++ locustfile.py
@@ -1,8 +1,21 @@
 import time
-from locust import HttpUser, task, between
+from locust import HttpUser, task, between, LoadTestShape
 from random import randint, choice, seed
 
 seed(3721)
+
+class CustomShapeLoad(LoadTestShape):
+    time_limit = 300
+    spawn_rate = 20
+    user_count = 250
+
+    def tick(self):
+        run_time = self.get_run_time()
+
+        if run_time < self.time_limit:
+            return (self.user_count, self.spawn_rate)
+
+        return None
 
 class BlogUser(HttpUser):
     wait_time = between(1, 2.5)
